dcea555eb28449af2eb8a9d75c58f3f841ea8aa1
SanyamAgrawal-tech <sanyamagrawal28@gmail.com>
2025-06-30 23:08:26 +0530

    Delete assignment4task1.py

--- assignment4task1.py
+++
@@ -1,8 +0,0 @@
-try:
-    with open('sample.txt','r') as file1:
-        l = file1.readlines()
-        print("Reading file content")
-        for i in range(len(l)):
-            print(f"Line {i+1}: {l[i]}")
-except:
-    print('Error: The file "sample.txt" was not found.')
